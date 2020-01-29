40783bc1d6a73ac191c018730563a2e5025ca2cd
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-01-29 09:05:45 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+const taskForm = document.querySelector("#create-task-form");
+
+taskForm.addEventListener("submit", handleFormSubmit)
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const form = event.target
+
+  const taskDescription = form["new-task-description"].value;
+
+  const newTask = {
+    description: taskDescription
+  };
+
+  addTask(newTask);
+};
+
+const taskList = document.querySelector("#list")
+
+function addTask(taskObj) {
+  const taskLi = document.createElement('li')
+    taskLi.className = "task"
+
+    taskLi.innerHTML = `
+      ${taskObj.description}
+    `
+    taskList.append(taskLi)
+}
+
 });
